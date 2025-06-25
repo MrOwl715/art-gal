ee2a062794d4e98fc154b577d244aeef5a5941a4
MrOwl715 <trannguyendinh.yhd@gmail.com>
2025-06-25 11:06:59 +0700

    first

---
+++ src/main/java/com/example/artgal/ArtgalApplication.java
@@ -0,0 +1,13 @@
+package com.example.artgal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtgalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtgalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artgal/ArtgalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artgal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtgalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
